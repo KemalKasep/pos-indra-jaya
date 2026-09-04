c181137d1e8398f3d22dc33769e804b2fd8297c2
Kemal <tayosqd12@gmail.com>
2026-09-04 09:24:27 +0700

    update offline

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // Membuka akses untuk jaringan lokal (HP/perangkat lain)
-    port: 5173
-  }
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Otomatis update aplikasi jika ada versi baru
+      manifest: {
+        name: 'Kasir Indra Jaya Pusat',
+        short_name: 'Indra Jaya',
+        description: 'Sistem POS Kasir Indra Jaya Pusat',
+        theme_color: '#1e293b',
+        background_color: '#f1f5f9',
+        display: 'standalone', // Membuatnya full screen tanpa address bar
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
