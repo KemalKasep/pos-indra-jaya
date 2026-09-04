b8ebab1857dda5b64619296936f4b134d5b51f15
Kemal <tayosqd12@gmail.com>
2026-09-04 10:09:11 +0700

    rollback darurat

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        navigateFallback: '/index.html' // Kunci untuk mengatasi blank screen di Vercel
-      },
-      manifest: {
-        name: 'Kasir Indra Jaya Pusat',
-        short_name: 'Indra Jaya',
-        description: 'Sistem POS Kasir Indra Jaya Pusat',
-        theme_color: '#1e293b',
-        background_color: '#f1f5f9',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
 })
